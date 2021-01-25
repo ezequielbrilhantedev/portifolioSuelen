edde0c09742a03222c07e28ade03695ce5f126a0
Ezequiel Brilhante <ezequielrb7@gmail.com>
2021-01-25 10:46:48 -0300

    Commit inicial, inicialização do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './home/components/navbar/navbar.component';
+import { PerfilComponent } from './home/components/perfil/perfil.component';
+import { TrabalhosComponent } from './home/components/trabalhos/trabalhos.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarComponent, PerfilComponent, TrabalhosComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
