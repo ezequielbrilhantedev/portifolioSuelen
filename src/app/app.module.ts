c0fc956054abe69a95eba4af46fcd38eac71d4c3
Ezequiel Brilhante <ezequielrb7@gmail.com>
2021-04-20 02:31:45 -0300

    Adição dos componentes do Material Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,27 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './home/components/navbar/navbar.component';
 import { PerfilComponent } from './home/components/perfil/perfil.component';
 import { TrabalhosComponent } from './home/components/trabalhos/trabalhos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, PerfilComponent, TrabalhosComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    PerfilComponent,
+    TrabalhosComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
